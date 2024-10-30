2f4b29b568a5b4b04bef2ecea1eb69a72981e556
cogitorp <paul.cogitore@etu.univ-grenoblealpes.fr>
2024-10-30 22:45:49 +0100

    maj JS page projets, scroll OK

--- script.js
+++ script.js
@@ -17,19 +17,63 @@ function scrollToLeft(value) {
     })
 }
 
+
 function hoverEffect(id) {
     const container = document.getElementById('projects-container');
-    setTimeout(() => {
-        if (!isElementFullyVisible(id)) {
-            let value = getHiddenPixelsRight(id) + 12;
-            setTimeout(() => {scrollToRight(value);}, 10); 
-        };
-    }, 40);
+    let projet = document.getElementById(id);
 
+    if ( projet.classList.contains('project-visible')) {
+        setTimeout(() => {
+            if (!isElementFullyVisibleRight(id)) {
 
-    
+                let value = getHiddenPixelsRight(id) + 11;
+                setTimeout(() => {scrollToRight(value);}, 10); 
+
+            }else if (!isElementFullyVisibleLeft(id)) {
+                let value = (getHiddenPixelsLeft(id) + 11);
+                
+                setTimeout(() => {scrollToLeft(value);}, 10);
+            };
+        }, 150);  
+    }
 }
 
+
+function isElementFullyVisibleRight(id) {
+    const container = document.getElementById('projects-container');
+    const element = document.getElementById(id);
+    // Obtenir les dimensions et positions de l'élément et du conteneur
+    const elementRect = element.getBoundingClientRect();
+    const containerRect = container.getBoundingClientRect();
+
+    // Vérifier si l'élément est complètement visible horizontalement dans le conteneur
+    
+    const isFullyVisible = (
+        elementRect.right <= containerRect.right 
+    );
+
+    
+
+    return isFullyVisible;
+}
+
+function isElementFullyVisibleLeft(id) {
+    const container = document.getElementById('projects-container');
+    const element = document.getElementById(id);
+    // Obtenir les dimensions et positions de l'élément et du conteneur
+    const elementRect = element.getBoundingClientRect();
+    const containerRect = container.getBoundingClientRect();
+
+    // Vérifier si l'élément est complètement visible horizontalement dans le conteneur
+    
+    const isFullyVisible = (
+        elementRect.left >= containerRect.left 
+    );
+
+    
+
+    return isFullyVisible;
+}
 
 function isElementFullyVisible(id) {
     const container = document.getElementById('projects-container');
@@ -41,6 +85,7 @@ function isElementFullyVisible(id) {
     // Vérifier si l'élément est complètement visible horizontalement dans le conteneur
     
     const isFullyVisible = (
+        elementRect.left >= containerRect.left &&
         elementRect.right <= containerRect.right
     );
 
@@ -64,4 +109,69 @@ function getHiddenPixelsRight(id) {
     }
 
     return hiddenPixelsRight;
+}
+
+function getHiddenPixelsLeft(id) {
+    const container = document.getElementById('projects-container');
+    const element = document.getElementById(id);
+    const elementRect = element.getBoundingClientRect();
+    const containerRect = container.getBoundingClientRect();
+
+    let hiddenPixelsLeft = 0;
+
+    // Vérifie si une partie de l'élément est cachée à droite
+    if (elementRect.left < containerRect.left) {
+        hiddenPixelsLeft = containerRect.left - elementRect.left;
+    }
+
+    return hiddenPixelsLeft;
+}
+
+function loadVisibleProject() {
+    let projects = document.getElementById('projects-container').children;
+    setTimeout(() => {
+        for (let i = 0; i < projects.length; i++) {
+            if (isElementFullyVisible(projects[i].id) && !projects[i].classList.contains('project-visible')) {
+
+                projects[i].classList.add('project-visible');
+
+            }else if (!isElementFullyVisible(projects[i].id)) {
+
+                projects[i].classList.remove('project-visible');
+            }
+
+        }
+    }, 250);
+}
+
+
+function canScroll() {
+    const container = document.getElementById('projects-container');
+    const projects = container.children;
+    let positionLast = projects[projects.length-1].getBoundingClientRect();
+
+    let scrollableRight = true;
+
+    if (positionLast.right <= container.getBoundingClientRect().right) {
+        scrollableRight = false;
+    }else{
+        scrollableRight = true
+    }
+    
+    let scrollableLeft = container.scrollLeft > 0;
+
+    return { scrollableLeft, scrollableRight };
+}
+
+function updateScrollButton() {
+    const buttonLeft = document.getElementById('scrollProjectLeft');
+    const buttonRight = document.getElementById('scrollProjectRight');
+
+    setTimeout(() => {
+        let { scrollableLeft, scrollableRight } = canScroll();
+
+        buttonLeft.disabled = !scrollableLeft; // Désactive le bouton si on ne peut pas scroller à gauche
+        buttonRight.disabled = !scrollableRight; // Désactive le bouton si on ne peut pas scroller à droite
+    }, 250);
+
 }
